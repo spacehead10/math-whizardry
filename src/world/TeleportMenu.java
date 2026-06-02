4768a23d048d3345a9a0a917fc804d1087d848d3
Rence <renceluo10@gmail.com>
2026-06-02 17:36:12 -0400

    Cumulus Acropolis
    Added biome and room files for Cumulus Acropolis.
    Added Cumulus Acropolis to teleport menu.
    Added a sample save file with all Relics, 10000 Gold, and level 18.

--- src/world/TeleportMenu.java
+++ src/world/TeleportMenu.java
@@ -21,6 +21,7 @@ public class TeleportMenu implements Values {
     public TeleportMenu(WorldState ws) {
         calderaCastleButton = new MapTeleportButton(getScreenWidth() / 2 - TELEPORT_BUTTON_WIDTH / 2, 20 + Media.defaultFontLarge.getHeight() + 10, World.calderaCastle(), ws);
         permafrostGlaciersButton = new MapTeleportButton(getScreenWidth() / 2 - TELEPORT_BUTTON_WIDTH / 2, 20 + Media.defaultFontLarge.getHeight() + 10 + TELEPORT_BUTTON_HEIGHT + 10, World.permafrostGlaciers(), ws);
+        cumulusAcropolisButton = new MapTeleportButton(getScreenWidth() / 2 - TELEPORT_BUTTON_WIDTH / 2, 20 + Media.defaultFontLarge.getHeight() + 10 + 2 * (TELEPORT_BUTTON_HEIGHT + 10), World.cumulusAcropolis(), ws);
     }
 
     public void render(Graphics g, GameContainer gc) {
@@ -30,10 +31,12 @@ public class TeleportMenu implements Values {
         Media.drawAlignedString("Travel to Biome", getScreenWidth() / 2, 20, Media.CENTER, Media.TOP, Media.defaultFontLarge, g);
         calderaCastleButton.render(g, gc);
         permafrostGlaciersButton.render(g, gc);
+        cumulusAcropolisButton.render(g, gc);
     }
 
     public void mousePressed(int button, int x, int y) {
         calderaCastleButton.mousePressed(x, y);
         permafrostGlaciersButton.mousePressed(x, y);
+        cumulusAcropolisButton.mousePressed(x, y);
     }
 }
